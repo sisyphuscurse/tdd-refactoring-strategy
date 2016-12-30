20b685ed9599862d1abe5a5100bd479a746f6a50
barry <zhongdejian@hfbank.com.cn>
2016-12-30 16:36:30 +0800

    按照相同方法完成绿头鸭测试规格以及声明绿头鸭实现类以及方法

---
+++ src/main/java/practice/tdd/refactoring/strategy/GreenHeadDuck.java
@@ -0,0 +1,19 @@
+package practice.tdd.refactoring.strategy;
+
+/**
+ * Created by zhongdj on 30/12/2016.
+ */
+public class GreenHeadDuck {
+
+    public String display() {
+        return null;
+    }
+
+    public String quack() {
+        return null;
+    }
+
+    public String swim() {
+        return null;
+    }
+}
--- src/test/java/practice/tdd/refactoring/strategy/DucksTest.java
+++ src/test/java/practice/tdd/refactoring/strategy/DucksTest.java
@@ -36,11 +36,29 @@ public class DucksTest {
     }
 
     @Test public void should_return_green_head_duck_when_display_a_green_head_duck() {
+        //given
+        final GreenHeadDuck theDuck = new GreenHeadDuck();
+        //when
+        final String displayed = theDuck.display();
+        //then
+        Assert.assertEquals("green head duck", displayed.toLowerCase());
     }
 
     @Test public void should_return_I_can_quack_when_green_head_duck_quacks() {
+        //given
+        final GreenHeadDuck theDuck = new GreenHeadDuck();
+        //when
+        final String quacked = theDuck.quack();
+        //then
+        Assert.assertEquals("I can quack", quacked.toLowerCase());
     }
 
     @Test public void should_return_I_can_swim_when_green_head_duck_swims() {
+        //given
+        final GreenHeadDuck theDuck = new GreenHeadDuck();
+        //when
+        final String swimmed = theDuck.swim();
+        //then
+        Assert.assertEquals("I can swim", swimmed.toLowerCase());
     }
 }
