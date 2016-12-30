c41848c14a63879259fc6635828a76b46d39530a
barry <zhongdejian@hfbank.com.cn>
2016-12-30 16:25:32 +0800

    通过Alt+Enter辅助生成源码类和相应方法

---
+++ src/main/java/practice/tdd/refactoring/strategy/RedHeadDuck.java
@@ -0,0 +1,11 @@
+package practice.tdd.refactoring.strategy;
+
+/**
+ * Created by zhongdj on 30/12/2016.
+ */
+public class RedHeadDuck {
+
+    public String display() {
+        return null;
+    }
+}
