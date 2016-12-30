5167613094b7968905406733e05f57df365dcec1
barry <zhongdejian@hfbank.com.cn>
2016-12-30 16:13:20 +0800

    建立测试用例Class并声明测试规格

---
+++ src/test/java/practice/tdd/refactoring/strategy/DucksTest.java
@@ -0,0 +1,27 @@
+package practice.tdd.refactoring.strategy;
+
+import org.junit.Test;
+
+/**
+ * Created by zhongdj on 30/12/2016.
+ */
+public class DucksTest {
+
+    @Test public void should_return_red_head_duck_when_display_a_red_head_duck() {
+    }
+
+    @Test public void should_return_I_can_quack_when_red_head_duck_quacks() {
+    }
+
+    @Test public void should_return_I_can_swim_when_red_head_duck_swims() {
+    }
+
+    @Test public void should_return_green_head_duck_when_display_a_green_head_duck() {
+    }
+
+    @Test public void should_return_I_can_quack_when_green_head_duck_quacks() {
+    }
+
+    @Test public void should_return_I_can_swim_when_green_head_duck_swims() {
+    }
+}
