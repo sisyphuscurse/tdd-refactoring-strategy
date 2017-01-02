2535dfd1c1bfa5a453249ad6661abb46f9db0cb1
barry <zhongdejian@hfbank.com.cn>
2017-01-02 20:31:18 +0800

    【代码生成】使用花键（Command）+ O在DecoyDuck类中插入覆盖父类的3个方法（提示:在窗口中通过Shift+Down选择多个方法,然后回车）

--- src/main/java/practice/tdd/refactoring/strategy/DecoyDuck.java
+++ src/main/java/practice/tdd/refactoring/strategy/DecoyDuck.java
@@ -9,5 +9,15 @@ public class DecoyDuck extends AbsDuck {
         return "decoy duck";
     }
 
-    
+    @Override public String quack() {
+        return super.quack();
+    }
+
+    @Override public String swim() {
+        return super.swim();
+    }
+
+    @Override public String fly() {
+        return super.fly();
+    }
 }
